ef747a8e23c6a04f9908a7efb88d77edb173e5c4
Joe Burdick <josephdburdick@gmail.com>
2013-07-24 02:35:54 -0400

    Switched to compressed JS file

--- inc/footer.php
+++ inc/footer.php
@@ -9,7 +9,7 @@
     <script src="js/vendor/jquery.easing-1.3.js"></script>
     <script src="js/vendor/jquery.royalslider.min.js"></script>
     <script src="js/vendor/vertCenter-ck.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main-ck.js"></script>
 
 
     <script>
