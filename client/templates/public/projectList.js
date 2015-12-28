f6b58e7512bb821136cb964f334ad38acc2bec78
Joe <josephdburdick@gmail.com>
2015-12-27 21:25:03 -0300

    Remove unnecessary template helpers from projectList.

--- client/templates/public/projectList.js
+++ client/templates/public/projectList.js
@@ -5,15 +5,5 @@ Template.projectList.onCreated(() => {
 Template.projectList.helpers({
   projects() {
     return Projects.find();
-  },
-  index(){
-    return Projects.find({"_id": {$lt: this._id}}).count() + 1;
-  },
-  projectListWidth(){
-    let projectCount = Projects.find().count(),
-        totalWidth = [];
-
-        debugger;
-
   }
 });
