22e04f41f121b28ee8820c902129b13641f47a56
Joe <josephdburdick@gmail.com>
2015-12-28 11:07:57 -0300

    Another step toward horizontal scrolling.

--- client/templates/public/projectList.js
+++ client/templates/public/projectList.js
@@ -15,41 +15,41 @@ Template.projectList.events({
   'mouseleave .projectPane': (ev) => {
     $(ev.currentTarget).toggleClass('hover');
   },
-  'mousewheel .projects-container': (ev) => {
+  'mousewheel .projects-container': (ev, template) => {
     console.log(ev);
-    // var projectPaneContainer, projectPane, projectCount, projectPaneWidth, projectPaneHeight;
-    // projectPaneWidth = $('#index .project').width(),
-    // projectPaneHeight = $('#index .project').height(),
-    // projectPaneContainer = $('.project-container'),
-    // projectPane = $('#index').find('.project'),
-    // projectCount = projectPane.length;
+    var projectPaneContainer, projectPane, projectCount, projectPaneWidth, projectPaneHeight;
+    projectPaneWidth = template.find('.projectPane').width(),
+    projectPaneHeight = template.find('.projectPane').height(),
+    projectPaneContainer = $('.project-container'),
+    projectPane = $('#index').find('.project'),
+    projectCount = projectPane.length;
 
-    // $('#index .project .info').vertCenter({percentage: 1.75});
+    template.find('.projectPane .info').vertCenter({percentage: 1.75});
 
-    // if (!Modernizr.touch){
-    //   if (windowWidth() >= tabletBreakPoint){
-    //     projectPaneContainer.width((projectCount * projectPaneWidth));
-    //     sectionIndex.unbind('mousewheel').mousewheel(function(event, delta) {
-    //       this.scrollLeft -= Math.round(parseInt(delta * 45));
-    //       return false;
-    //     });
-    //   } else {
-    //     sectionIndex.unbind('mousewheel');
-    //   }
-    // } else {
-    //     projectPaneContainer.width((projectCount * projectPaneWidth));
-    // }
-    // if (mobile() && windowWidth() <= tabletBreakPoint){
-    //   $('.project').addClass('hover');
-    // } else {
-    //   $('.project').on('mouseenter',function(){
-    //     $(this).addClass('hover latest').siblings().removeClass('hover latest');
-    //   }).on('mouseleave',function(){
-    //     $(this).removeClass('hover').addClass('latest');
-    //   });
-    //   nav.on('mouseenter',function(){
-    //     $(this).click();
-    //   });
-    // }
+    if (!Modernizr.touch){
+      if (windowWidth() >= tabletBreakPoint){
+        projectPaneContainer.width((projectCount * projectPaneWidth));
+        sectionIndex.unbind('mousewheel').mousewheel(function(event, delta) {
+          this.scrollLeft -= Math.round(parseInt(delta * 45));
+          return false;
+        });
+      } else {
+        sectionIndex.unbind('mousewheel');
+      }
+    } else {
+        projectPaneContainer.width((projectCount * projectPaneWidth));
+    }
+    if (mobile() && windowWidth() <= tabletBreakPoint){
+      $('.project').addClass('hover');
+    } else {
+      $('.project').on('mouseenter',function(){
+        $(this).addClass('hover latest').siblings().removeClass('hover latest');
+      }).on('mouseleave',function(){
+        $(this).removeClass('hover').addClass('latest');
+      });
+      nav.on('mouseenter',function(){
+        $(this).click();
+      });
+    }
   }
 });
