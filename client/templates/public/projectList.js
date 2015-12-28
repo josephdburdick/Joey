076e6f51b2987e06bc95d3dabad59044c2614869
Joe <josephdburdick@gmail.com>
2015-12-27 22:28:27 -0300

    Add mousewheel detection.

--- client/templates/public/projectList.js
+++ client/templates/public/projectList.js
@@ -14,5 +14,42 @@ Template.projectList.events({
   },
   'mouseleave .projectPane': (ev) => {
     $(ev.currentTarget).toggleClass('hover');
+  },
+  'mousewheel .projects-container': (ev) => {
+    console.log(ev);
+    // var projectPaneContainer, projectPane, projectCount, projectPaneWidth, projectPaneHeight;
+    // projectPaneWidth = $('#index .project').width(),
+    // projectPaneHeight = $('#index .project').height(),
+    // projectPaneContainer = $('.project-container'),
+    // projectPane = $('#index').find('.project'),
+    // projectCount = projectPane.length;
+
+    // $('#index .project .info').vertCenter({percentage: 1.75});
+
+    // if (!Modernizr.touch){
+    //   if (windowWidth() >= tabletBreakPoint){
+    //     projectPaneContainer.width((projectCount * projectPaneWidth));
+    //     sectionIndex.unbind('mousewheel').mousewheel(function(event, delta) {
+    //       this.scrollLeft -= Math.round(parseInt(delta * 45));
+    //       return false;
+    //     });
+    //   } else {
+    //     sectionIndex.unbind('mousewheel');
+    //   }
+    // } else {
+    //     projectPaneContainer.width((projectCount * projectPaneWidth));
+    // }
+    // if (mobile() && windowWidth() <= tabletBreakPoint){
+    //   $('.project').addClass('hover');
+    // } else {
+    //   $('.project').on('mouseenter',function(){
+    //     $(this).addClass('hover latest').siblings().removeClass('hover latest');
+    //   }).on('mouseleave',function(){
+    //     $(this).removeClass('hover').addClass('latest');
+    //   });
+    //   nav.on('mouseenter',function(){
+    //     $(this).click();
+    //   });
+    // }
   }
 });
