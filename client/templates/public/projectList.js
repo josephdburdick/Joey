2901faa41b9d8afb7fab33582005efc1ba5938d6
Joe <josephdburdick@gmail.com>
2015-12-29 10:07:03 -0300

    Implement horizontal scrolling for projectList.

--- client/templates/public/projectList.js
+++ client/templates/public/projectList.js
@@ -1,5 +1,13 @@
 Template.projectList.onCreated(() => {
   Template.instance().subscribe('allProjects');
+
+	let $body = $('body');
+
+	$body.on('mousewheel', function(event, delta) {
+		console.log(event, delta);
+		$body[0].scrollLeft -= Math.round(parseInt(delta * 45));
+		return false;
+	});
 });
 
 Template.projectList.helpers({
@@ -14,42 +22,5 @@ Template.projectList.events({
   },
   'mouseleave .projectPane': (ev) => {
     $(ev.currentTarget).toggleClass('hover');
-  },
-  'mousewheel .projects-container': (ev, template) => {
-    console.log(ev);
-    var projectPaneContainer, projectPane, projectCount, projectPaneWidth, projectPaneHeight;
-    projectPaneWidth = template.find('.projectPane').width(),
-    projectPaneHeight = template.find('.projectPane').height(),
-    projectPaneContainer = $('.project-container'),
-    projectPane = $('#index').find('.project'),
-    projectCount = projectPane.length;
-
-    template.find('.projectPane .info').vertCenter({percentage: 1.75});
-
-    if (!Modernizr.touch){
-      if (windowWidth() >= tabletBreakPoint){
-        projectPaneContainer.width((projectCount * projectPaneWidth));
-        sectionIndex.unbind('mousewheel').mousewheel(function(event, delta) {
-          this.scrollLeft -= Math.round(parseInt(delta * 45));
-          return false;
-        });
-      } else {
-        sectionIndex.unbind('mousewheel');
-      }
-    } else {
-        projectPaneContainer.width((projectCount * projectPaneWidth));
-    }
-    if (mobile() && windowWidth() <= tabletBreakPoint){
-      $('.project').addClass('hover');
-    } else {
-      $('.project').on('mouseenter',function(){
-        $(this).addClass('hover latest').siblings().removeClass('hover latest');
-      }).on('mouseleave',function(){
-        $(this).removeClass('hover').addClass('latest');
-      });
-      nav.on('mouseenter',function(){
-        $(this).click();
-      });
-    }
   }
 });
