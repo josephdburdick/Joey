ce85c053138fe6bd6d2c52a4f7a1f2aed6ccf276
Joe <josephdburdick@gmail.com>
2015-12-30 11:50:44 -0300

    Remove unnecessary modules.

--- client/modules/startup.js
+++ client/modules/startup.js
@@ -1,18 +1,8 @@
 let startup = () => {
   Meteor.startup(function () {
-    GoogleMaps.load();
-    _geolocation();
-    _setGeolocation();
-    _markerClusterer();
     _UI();
   });
 };
-
-var _geolocation = () => Modules.client.Geolocation;
-
-var _setGeolocation = () => Modules.client.setGeolocation();
-
-var _markerClusterer = () => Modules.client.markerClusterer();
 
 var _UI = () => Modules.client.UI;
 
