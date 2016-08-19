d21b901d5540350f96804f64385a7201767d23c8
Joe Burdick <josephdburdick@gmail.com>
2016-08-19 19:43:40 -0400

    Remove index page and rename projects to index.

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,21 +3,75 @@ import {Link} from 'react-router'
 import sortBy from 'lodash/sortBy'
 import DocumentTitle from 'react-document-title'
 import {prefixLink} from 'gatsby-helpers'
+import {rhythm} from 'utils/typography'
 import access from 'safe-access'
 import {config} from 'config'
-import Projects from './Projects';
+import include from 'underscore.string/include'
+import $ from 'jquery';
 
-class Index extends React.Component {
+
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrolling: false
+    };
+    this.scrollHorizontal = this.scrollHorizontal.bind(this);
+  }
+  scrollHorizontal(event) {
+    console.log(event);
+  }
+  onMouseEnter(event) {
+    console.log(event);
+  }
+  onMouseLeave(event) {
+    console.log(event);
+  }
   render() {
-    const route = this.props.route;
+    const $window = $(window);
+    const projectLinks = []
+
+    // Sort projects.
+    const sortedPages = sortBy(this.props.route.pages, (project) => access(project, 'data.order')).reverse();
+    const totalPages = sortedPages.length - 1;
+    console.log(totalPages);
+
+    $window.on('mousewheel', (event) => this.scrollHorizontal(event));
+
+    sortedPages.forEach((project) => {
+      if (access(project, 'file.ext') === 'md' && !include(project.path, '/404')) {
+        const projectNumber = totalPages - project;
+        const title = access(project, 'data.title') || project.path;
+        const logo = project.path + access(project, 'data.logo');
+        const bgImage = project.path + 'bg.jpg';
+        const slideCount = access(project, 'data.slides').length;
+
+        projectLinks.push(
+          <li key={project.path} className="project-item" style={{backgroundImage: `url(${bgImage})`}} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+            <Link to={prefixLink(project.path)} className="project-item__link">
+              <div className="card">
+                <div class="card-header">{title}</div>
+                <img src={logo} />
+              </div>
+            </Link>
+          </li>
+        )
+      }
+    })
     return (
-      <Projects {...this.props} />
+      <main>
+        <div className="projects-list-container" id="scroll-horizontal">
+          <ul className="projects-list">
+            { projectLinks }
+          </ul>
+        </div>
+      </main>
     )
   }
 }
 
-Index.propTypes = {
+Projects.propTypes = {
   route: React.PropTypes.object
 }
 
-export default Index
+export default Projects
