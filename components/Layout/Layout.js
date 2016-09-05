c87513902048a9625420916b93cadfba9d3fea0b
Joe Burdick <josephdburdick@gmail.com>
2016-09-05 12:09:13 -0400

    Toggle window resize/scroll in component state.

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -20,7 +20,7 @@ class Layout extends React.Component {
 
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout" ref={node => (this.root = node)}>
+      <div ref={node => (this.root = node)}>
         <div className="mdl-layout__inner-container">
           <main>
             <div {...this.props} className={cx(s.content, this.props.className)} />
