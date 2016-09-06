685eb6d1547da100b92e390176eb7524dd9c1b71
Joe Burdick <josephdburdick@gmail.com>
2016-09-06 15:19:43 -0400

    Create initial ProjectCard component.

---
+++ components/Slider/ProjectCard.js
@@ -0,0 +1,58 @@
+import React, {PropTypes} from 'react';
+
+class ProjectCard extends React.Component{
+  static propTypes = {
+    project: PropTypes.shape({
+      order: PropTypes.number,
+      name: PropTypes.string,
+      title: PropTypes.string,
+      data: PropTypes.date,
+      layout: PropTypes.string,
+      tags: PropTypes.arrayOf(PropTypes.string),
+      logo: PropTypes.string,
+      agency: PropTypes.string,
+      slides: PropTypes.arrayOf(PropTypes.string)
+    })
+  }
+  render(){
+    const project = this.props.project;
+    const projectLink = `project/${project.name}`;
+    const projectTags = project.tags
+    ? (
+      <ul className="project-tags">
+      {project.tags.map((tag, i) => (
+        <li key={i}>{tag}</li>
+      ))}
+      </ul>
+    )
+    : null;
+    const projectNumber = ('0' + (project.order + 1)).slice(-2);
+    return (
+      <li className="project" id={`project-card__${project.name}`}>
+        <div className="demo-card-square mdl-card mdl-shadow--2dp">
+          <div className="mdl-card__title mdl-card--expand">
+            <h2 className="mdl-card__title-text">{project.title}</h2>
+          </div>
+          <div className="mdl-card__supporting-text">
+            <div className="info">
+              <header>
+                <div className="project-number">{projectNumber}</div>
+                <div className="project-name">{project.title}</div>
+                <hr/>
+                {projectTags}
+              </header>
+            </div>
+          </div>
+          <div className="mdl-card__actions mdl-card--border">
+            <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
+              href={projectLink}>
+              View Project
+            </a>
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default ProjectCard;
--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import ProjectCard from './ProjectCard.js';
 
 class Slider extends React.Component {
   static propTypes = {
@@ -16,44 +17,8 @@ class Slider extends React.Component {
   }
   render() {
     const projects = Object.values(this.props.slides).sort((a, b) => a.order - b.order);
-    const projectCard = (project) => {
-      console.log(project);
-      const projectTags = project.tags
-        ? (
-          <ul className="project-tags">
-            {project.tags.map((tag) => (
-              <li>{tag}</li>
-            ))}
-          </ul>
-        )
-        : null;
-      const projectNumber = ('0' + (project.order + 1)).slice(-2)
-      return (
-        <li className="project" id={`project-card__${project.name}`}>
-          <div className="demo-card-square mdl-card mdl-shadow--2dp">
-            <div className="mdl-card__title mdl-card--expand">
-              <h2 className="mdl-card__title-text">{project.title}</h2>
-            </div>
-            <div className="mdl-card__supporting-text">
-              <div className="info">
-                <header>
-                  <div className="project-number">{projectNumber}</div>
-                  <div className="project-name">{project.title}</div>
-                  <hr/>
-                  {projectTags}
-                </header>
-              </div>
-            </div>
-            <div className="mdl-card__actions mdl-card--border">
-              <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                View Project
-              </a>
-            </div>
-          </div>
-        </li>
-      )
-    };
-    const slides = projects.map((project) => projectCard(project));
+    const slides = projects.map((project, i) => (<ProjectCard key={i} project={project}/>));
+
     return (
       <div>
         <ul>
