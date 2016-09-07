9e0bd9fe2fb3555dce93aff0c70b374d288f13ae
Joe Burdick <josephdburdick@gmail.com>
2016-09-06 21:34:06 -0400

    Add lateral movement of slider track with pageX.

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -18,7 +18,7 @@ class Slider extends React.Component {
       pageY: 0,
       offsetX: 0,
       offsetY: 0,
-      totalWidth: 0,
+      trackWidth: 0,
       style: {
         left: '0px'
       }
@@ -26,20 +26,32 @@ class Slider extends React.Component {
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
+  componentDidMount(){
+    this.setState({
+      trackWidth: $(this.refs.slider).outerWidth(true)
+    })
+    console.log(this.state.trackWidth);
+  }
   handleMouseEnter(e){
     this.setState({
-      totalWidth: $(e.currentTarget).outerWidth()
+      trackWidth: $(this.refs.slider).outerWidth(true)
     });
+    console.log(this.state.trackWidth);
   }
   handleMouseMove(e){
+    // debugger;
+    let percentage = Math.ceil((e.pageX / this.props.windowWidth) * 80);
+    console.log((percentage / this.state.trackWidth) * 100);
+    // percentage = percentage > 95 ? 105 : percentage;
+    // percentage = percentage < 5 ? -10 : percentage;
+    // console.log(percentage);
     this.setState({
       pageX: e.pageX,
       pageY: e.pageY,
       style: prefix({
-        transform: `translateX(-${e.pageX}px)`
+        transform: `translateX(-${percentage}%)`
       })
     });
-    console.log(this.state.style);
   }
   render() {
     const projects = Object.values(this.props.slides).sort((a, b) => a.order - b.order);
@@ -51,7 +63,7 @@ class Slider extends React.Component {
         <div dangerouslySetInnerHTML={{
           __html: html
         }}/>
-        <div className={s.slider} onMouseEnter={this.handleMouseEnter} onMouseMove={this.handleMouseMove}>
+        <div className={s.slider} onMouseEnter={this.handleMouseEnter} onMouseMove={this.handleMouseMove} ref="slider">
           <ul style={this.state.style}>
             {slides}
           </ul>
