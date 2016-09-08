dab0c6624a1c49d4f7e03c1a19bfd6d1eca87fde
Joe Burdick <josephdburdick@gmail.com>
2016-09-07 20:54:49 -0400

    Replace custom horizontal scroll with forked version of react-scroll-horizontal.

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -3,70 +3,36 @@ import prefix from 'react-prefixer';
 import s from './Slider.css';
 import ProjectCard from '../ProjectCard/ProjectCard.js';
 import {title, html} from './slider.md';
-import $ from 'jquery';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 class Slider extends React.Component {
   static propTypes = {
-    windowHeight: PropTypes.number.isRequired,
-    windowWidth: PropTypes.number.isRequired,
-    slides: PropTypes.object
+    slides: PropTypes.object,
+    windowWidth: PropTypes.number
   }
-  constructor(props){
-    super(props);
-    this.state = {
-      pageX: 0,
-      pageY: 0,
-      offsetX: 0,
-      offsetY: 0,
-      trackWidth: 0,
-      style: {
-        left: '0px'
-      }
-    }
-    this.handleMouseEnter = this.handleMouseEnter.bind(this);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-  }
-  componentDidMount(){
-    this.setState({
-      trackWidth: $(this.refs.slider).outerWidth(true)
-    })
-    console.log(this.state.trackWidth);
-  }
-  handleMouseEnter(e){
-    this.setState({
-      trackWidth: $(this.refs.slider).outerWidth(true)
-    });
-    console.log(this.state.trackWidth);
-  }
-  handleMouseMove(e){
-    // debugger;
-    let percentage = Math.ceil((e.pageX / this.props.windowWidth) * 80);
-    console.log((percentage / this.state.trackWidth) * 100);
-    // percentage = percentage > 95 ? 105 : percentage;
-    // percentage = percentage < 5 ? -10 : percentage;
-    // console.log(percentage);
-    this.setState({
-      pageX: e.pageX,
-      pageY: e.pageY,
-      style: prefix({
-        transform: `translateX(-${percentage}%)`
-      })
-    });
-  }
+
   render() {
     const projects = Object.values(this.props.slides).sort((a, b) => a.order - b.order);
-    const slides = projects.map((project, i) => <li className={s['project-card']} key={i}><ProjectCard project={project}/></li>);
-
+    const renderSlides = projects.map((project, i) => <ProjectCard project={project} className={s['project-card']} key={i}/>);
+    const renderScrollArea = this.props.windowWidth > 768 ? (
+      <HorizontalScroll reverseScroll={true} pageLock={false}>
+        {renderSlides}
+      </HorizontalScroll>
+    ) : (
+      <div className={s['project-list--mobile']}>
+        {renderSlides}
+      </div>
+    )
     return (
       <div>
-        <h3>{title}</h3>
-        <div dangerouslySetInnerHTML={{
-          __html: html
-        }}/>
-        <div className={s.slider} onMouseEnter={this.handleMouseEnter} onMouseMove={this.handleMouseMove} ref="slider">
-          <ul style={this.state.style}>
-            {slides}
-          </ul>
+        <div className="container">
+          <h3>{title}</h3>
+          <div dangerouslySetInnerHTML={{
+            __html: html
+          }}/>
+        </div>
+        <div className={s.slider}>
+          {renderScrollArea}
         </div>
       </div>
     )
