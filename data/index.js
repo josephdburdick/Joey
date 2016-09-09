6538248e6558452430548e05c299c3fa3bb78e95
Joe Burdick <josephdburdick@gmail.com>
2016-09-08 22:06:02 -0400

    Use constuctor pattern to refactor and decouple the retrieval of project data.

---
+++ data/index.js
@@ -0,0 +1,25 @@
+import aoany from '../data/aoa-ny/aoa-ny.md';
+import bluechairbay from '../data/bluechairbay/bluechairbay.md';
+import ciroc from '../data/ciroc/ciroc.md';
+import citizen from '../data/citizen/citizen.md';
+import davidsbridal from '../data/davids-bridal/davids-bridal.md';
+import gshock from '../data/g-shock/g-shock.md';
+import hard from '../data/hard/hard.md';
+import macys from '../data/macys/macys.md';
+import mtv from '../data/mtv/mtv.md';
+import tambaran from '../data/tambaran/tambaran.md';
+import wwglass from '../data/wwglass/wwglass.md';
+
+export default {
+  aoany,
+  bluechairbay,
+  ciroc,
+  citizen,
+  davidsbridal,
+  gshock,
+  hard,
+  macys,
+  mtv,
+  tambaran,
+  wwglass
+}
