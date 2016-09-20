690f463d3f3c8e1948d2506aa6702581acd4f05b
Joe Burdick <josephdburdick@gmail.com>
2016-09-19 21:25:13 -0400

    Switch to hash routing.

--- core/history.js
+++ core/history.js
@@ -1,4 +1,5 @@
 import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createHashHistory from 'history/lib/createHashHistory';
 import useQueries from 'history/lib/useQueries';
 
-export default useQueries(createBrowserHistory)();
+export default useQueries(createHashHistory)();
