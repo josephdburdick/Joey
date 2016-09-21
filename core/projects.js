32defba933c8f7966aef872f6e828d479c777477
Joe Burdick <josephdburdick@gmail.com>
2016-09-21 18:33:22 -0400

    Add next and previous project data retrieval.

--- core/projects.js
+++ core/projects.js
@@ -10,6 +10,16 @@ const projects = {
   sorted(){ return Object.values(this.all).sort((a, b) => a.order - b.order) },
   get(project) {
     return this.all[project] || false;
+  },
+  previousProject(currentProject){
+    const currentProjectIndex = this.sorted().indexOf(currentProject);
+    const newIndex = !!(this.sorted()[currentProjectIndex - 1]) ? this.sorted().indexOf(this.sorted()[currentProjectIndex - 1]) : this.sorted().indexOf(this.sorted()[0]);
+    return this.sorted()[newIndex];
+  },
+  nextProject(currentProject){
+    const currentProjectIndex = this.sorted().indexOf(currentProject);
+    const newIndex = !!(this.sorted()[currentProjectIndex + 1]) ? this.sorted().indexOf(this.sorted()[currentProjectIndex + 1]) : this.sorted().indexOf(this.sorted()[this.sorted().length]);
+    return this.sorted()[newIndex];
   }
 };
 
