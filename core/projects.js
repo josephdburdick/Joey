f3f4672c1379aec48e703c43826111ade622c7b3
Joe Burdick <josephdburdick@gmail.com>
2016-09-21 13:53:09 -0400

    Replace map use with reduce on projects.

--- core/projects.js
+++ core/projects.js
@@ -1,18 +1,15 @@
 import projectsData from '../data/index.js';
 import { Project } from './createProject';
 
-let all = {};
-Object.values(projectsData).map(project => all[project.name] = new Project(project));
+const all = Object.values(projectsData).reduce((all, project) => {
+  return { ...all, [project.name]: new Project(project)};
+}, {});
 
 const projects = {
   all,
   sorted(){ return Object.values(this.all).sort((a, b) => a.order - b.order) },
   get(project) {
-    if (this.all[project]){
-      return this.all[project];
-    } else {
-      return false;
-    }
+    return this.all[project] || false;
   }
 };
 
