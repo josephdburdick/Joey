21b5c4a257bd0ba5e77faadcac5cff12b6d1c340
Joe Burdick <josephdburdick@gmail.com>
2017-07-11 16:10:46 -0400

    Comment out date info from project detail view.

--- pages/project/Project.js
+++ pages/project/Project.js
@@ -1,11 +1,6 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import 'whatwg-fetch';
-import moment from 'moment';
-import Layout from '../../components/Layout';
-import Link from '../../components/Link';
 import s from './project.css';
-import projects from '../../core/projects.js';
-// import LazyLoad from 'react-lazyload';
 
 class Project extends React.Component {
   componentDidMount() {
@@ -16,66 +11,116 @@ class Project extends React.Component {
   }
 
   render() {
-    const renderProjectSlides = this.props.project.slides.map((slide, i) => {
-      const imageUrl = !this.props.isMobile && window.devicePixelRatio > 1 || !this.props.isMobile && window.innerWidth > 1200
-        ? ([
-          slide.split('.')[0],
-          this.props.project.hiDefAffix,
-          '.',
-          slide.split('.')[1]
-        ].join(''))
-        : slide;
-      return (
-        <div className={`mdl-card mdl-shadow--2dp ${s.slide}`} key={i}>
-          <img src={this.props.project.slidesPath + imageUrl}/>
-        </div>
-      );
-    });
-    const renderProjectTags = this.props.project.tags.map((tag, i) => (
-      <span className="mdl-chip" key={i}>
-        <span className="mdl-chip__text">{tag}</span>
-      </span>
-    ));
+    const { project, isMobile } = this.props;
+    const renderTags =
+      project && project.tags
+        ? <section>
+            <div className={s.heading}>tags</div>
+            <div className="section-body">
+              {project.tags.map((tag, i) =>
+                <span className="mdl-chip" key={i}>
+                  <span className="mdl-chip__text">
+                    {tag}
+                  </span>
+                </span>
+              )}
+            </div>
+          </section>
+        : null;
+
+    const renderTitle =
+      project && project.title
+        ? <section>
+            <div className={s.heading}>name</div>
+            <div className="section-body">
+              {project.title}
+            </div>
+          </section>
+        : null;
+
+    const renderAgency =
+      project && project.agency
+        ? <section>
+            <div className={s.heading}>agency</div>
+            <div className="section-body">
+              {project.agency}
+            </div>
+          </section>
+        : null;
+
+    const renderDescription =
+      project && project.html
+        ? <section>
+            <div className={s.heading}>description</div>
+            <div
+              className="section-body"
+              dangerouslySetInnerHTML={{ __html: project.html }}
+            />
+          </section>
+        : null;
+
+    const renderProjectSlides =
+      project && project.slides.length
+        ? project.slides.map((slide, i) => {
+            const imageUrl =
+              (!isMobile && window.devicePixelRatio > 1) ||
+              (!isMobile && window.innerWidth > 1200)
+                ? [
+                    slide.split('.')[0],
+                    project.hiDefAffix,
+                    '.',
+                    slide.split('.')[1]
+                  ].join('')
+                : slide;
+            return (
+              <div className={`mdl-card mdl-shadow--2dp ${s.slide}`} key={i}>
+                <img src={project.slidesPath + imageUrl} />
+              </div>
+            );
+          })
+        : null;
     return (
       <section ref={node => (this.root = node)}>
         <div className={s.container}>
           <aside className={s.aside} ref="aside">
             <div>
-              <section>
-                <div className={s.heading}>name</div>
-                <div className="section-body">{this.props.project.title}</div>
-              </section>
-              <section>
-                <div className={s.heading}>date</div>
-                <div className="section-body">{moment(new Date(this.props.project.date)).format('MMMM YYYY')}</div>
-              </section>
-              <section>
-                <div className={s.heading}>agency</div>
-                <div className="section-body">{this.props.project.agency}</div>
-              </section>
-              <section>
-                <div className={s.heading}>description</div>
-                <div className="section-body" dangerouslySetInnerHTML={{__html: this.props.project.html }} />
-              </section>
-              <section>
-                <div className={s.heading}>tags</div>
-                <div className="section-body">
-                  {renderProjectTags}
-                </div>
-              </section>
+              {renderTitle}
+              {/*{renderDate}*/}
+              {renderAgency}
+              {renderDescription}
+              {renderTags}
             </div>
           </aside>
           <div className={s.slideshow} ref="slideshow">
             <div
-              className={`mdl-card mdl-shadow--2dp ${s.slide} ${s['logo-slide']}`}
-              style={{backgroundColor: this.props.project.color}}>
-              <img src={this.props.project.route + '/' + this.props.project.logo} alt={`${this.props.project.title} logo`} width="350"/></div>
+              className={`mdl-card mdl-shadow--2dp ${s.slide} ${s[
+                'logo-slide'
+              ]}`}
+              style={{ backgroundColor: project.color }}
+            >
+              <img
+                src={project.route + '/' + project.logo}
+                alt={`${project.title} logo`}
+                width="350"
+              />
+            </div>
             {renderProjectSlides}
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
-
-export default Project
+Project.propTypes = {
+  isMobile: PropTypes.bool,
+  project: PropTypes.shape({
+    title: PropTypes.string,
+    agency: PropTypes.string,
+    html: PropTypes.string,
+    slides: PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+    ),
+    tags: PropTypes.arrayOf(PropTypes.string)
+  })
+};
+export default Project;
