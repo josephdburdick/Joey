9dc93bf5932addd0d5d0d03974214b658472af7f
Joseph Burdick <josephdburdick@gmail.com>
2017-11-15 11:27:02 -0500

    Fix horizontal slider on touch devices.

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,21 +1,35 @@
-import React, {PropTypes} from 'react';
-import prefix from 'react-prefixer';
+import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import s from './Slider.css';
-import ProjectCard from '../ProjectCard/ProjectCard.js';
-import {title, html} from './slider.md';
+
 import HorizontalScroll from 'react-scroll-horizontal';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+const throttle = (type, name, obj) => {
+  obj = obj || window;
+  let running = false;
+  const func = () => {
+    if (running) {
+      return;
+    }
+    running = true;
+    requestAnimationFrame(() => {
+      obj.dispatchEvent(new CustomEvent(name));
+      running = false;
+    });
+  };
+  obj.addEventListener(type, func);
+};
 
 class Slider extends React.Component {
   static propTypes = {
     slides: PropTypes.array,
-    className: PropTypes.string
-  }
+    className: PropTypes.string,
+    isMobile: PropTypes.bool,
+  };
   constructor(props) {
     super(props);
     this.state = {
-      windowWidth: this.props.windowWidth
+      windowWidth: this.props.windowWidth,
     };
     this.handleResize = this.handleResize.bind(this);
   }
@@ -23,56 +37,38 @@ class Slider extends React.Component {
   componentDidMount() {
     this._isMounted = true;
     (() => {
-      const throttle = (type, name, obj) => {
-        obj = obj || window;
-        let running = false;
-        const func = function () {
-          if (running) {
-            return;
-          }
-          running = true;
-          requestAnimationFrame(function () {
-            obj.dispatchEvent(new CustomEvent(name));
-            running = false;
-          });
-        };
-        obj.addEventListener(type, func);
-      };
-
       throttle('resize', 'optimizedResize');
     })();
 
     window.addEventListener('optimizedResize', this.handleResize);
   }
 
+  componentWillUnmount() {
+    this._isMounted = false;
+    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   handleResize(e) {
     if (this._isMounted) {
       this.setState({
-        windowWidth: window.innerWidth
+        windowWidth: window.innerWidth,
       });
     }
   }
 
   render() {
-    const renderScrollArea = !this.state.isMobile && window.innerWidth > 768 ? (
-      <HorizontalScroll reverseScroll={true}>
-        {this.props.slides}
-      </HorizontalScroll>
-    ) : (
-      <div className={cx('scroll-horizontal', this.props.className ? this.props.className : null)}>
-        {this.props.slides}
-      </div>
-    );
-    return (
-      <div className={s.slider}>
-        {renderScrollArea}
-      </div>
-    )
-  }
-  componentWillUnmount() {
-    this._isMounted = false;
-    window.removeEventListener('resize', this.handleResize);
-    window.removeEventListener('scroll', this.handleScroll);
+    const renderScrollArea =
+      !this.props.isMobile && window.innerWidth > 768 ? (
+        <HorizontalScroll reverseScroll>{this.props.slides}</HorizontalScroll>
+      ) : (
+        <div
+          className={cx('scroll-horizontal', this.props.className ? this.props.className : null)}
+        >
+          {this.props.slides}
+        </div>
+      );
+    return <div className={s.slider}>{renderScrollArea}</div>;
   }
 }
 
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,8 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import Layout from '../../components/Layout';
 import s from './home.css';
-import {title, html} from './index.md';
+import { title, html } from './index.md';
 import history from '../../core/history.js';
 import Slider from '../../components/Slider/Slider';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
@@ -10,32 +10,50 @@ import projects from '../../core/projects';
 import MobileDetect from 'mobile-detect';
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile: false,
+    };
+  }
   componentDidMount() {
     this._isMounted = true;
     document.title = title;
+    this.handleMount();
+  }
+  handleMount() {
     setTimeout(() => {
       this.refs.projects.classList.add(s['projects--mounted']);
     }, 200);
   }
-
-  goRoute(event){
+  goRoute(event) {
     event.preventDefault();
     const { pathname } = event.currentTarget;
-    history.push({pathname})
+    history.push({ pathname });
   }
 
   render() {
-    const renderSlides = projects.sorted().map((project, i) => <ProjectCard project={project} className={s['project-card']} key={i} goRoute={this.goRoute} />);
-    const md = new MobileDetect(window.navigator.userAgent);
+    const renderSlides = projects
+      .sorted()
+      .map((project, i) => (
+        <ProjectCard
+          project={project}
+          className={s['project-card']}
+          key={i}
+          goRoute={this.goRoute}
+        />
+      ));
+    const isMobile = 'ontouchstart' in window.document.documentElement;
 
     return (
       <div className={s.content}>
         <section className={s.projects} ref="projects">
           <Slider
-            isMobile={!!md.mobile()}
+            isMobile={isMobile}
             isHighDef={window.devicePixelRatio > 1}
-            slides={ renderSlides }
-            className={s.slider} />
+            slides={renderSlides}
+            className={s.slider}
+          />
         </section>
       </div>
     );
